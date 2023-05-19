56a8824520be205388b3a80f38ffb834919627a5
ismailatac <atac571@outlook.com>
2023-05-19 16:17:20 +0300

    sale service and product service add

---
+++ sale-service/src/main/java/com/kodlamaio/saleservice/SaleServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kodlamaio.saleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaleServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaleServiceApplication.class, args);
+	}
+
+}
---
+++ sale-service/src/test/java/com/kodlamaio/saleservice/SaleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodlamaio.saleservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaleServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
